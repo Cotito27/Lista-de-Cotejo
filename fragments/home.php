150e5d8a3d6c3280b46af2e1826cfab889329f7a
Jesus <dotero27022001@gmail.com>
2020-07-23 14:11:21 -0500

    version 1

---
+++ fragments/home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="../css/home.css">
+</head>
+<body>
+    <h1>TAREAS RECIBIDAS</h1>
+<a href="home.php"><p id="notificacion">El alumno Rodrigo ha enviado su respuesta</p></a>
+<a href="home.php"><p id="notificacion">El alumno Rodrigo ha enviado su respuesta</p></a>
+<a href="home.php"><p id="notificacion">El alumno Rodrigo ha enviado su respuesta</p></a>
+<a href="home.php"><p id="notificacion">El alumno Rodrigo ha enviado su respuesta</p></a>
+<a href="home.php"><p id="notificacion">El alumno Rodrigo ha enviado su respuesta</p></a>
+<a href="home.php"><p id="notificacion">El alumno Rodrigo ha enviado su respuesta</p></a>
+</body>
+</html>
